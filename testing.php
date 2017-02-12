97833ab3f7c2a98ead98236a925908bf15588523
Jack Ross, III <mrjackross@gmail.com>
2017-02-11 21:51:28 -0500

    Changed namespacing to match new AccountKit namespacing

--- testing.php
+++ testing.php
@@ -4,5 +4,5 @@ ini_set('display_errors', 1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$accountKit = new \AccountKit\Controller\AccountKit();
+$accountKit = new \AccountKit\Controller();
 var_dump($accountKit);
