a4d41e7fdb67a70d844ff3808a9579054e6f20c8
Jack Ross, III <mrjackross@gmail.com>
2017-02-28 18:15:22 -0500

    Added page with validation and style sheet for allowing the user to enter their name

--- web/getUserName.php
+++ web/getUserName.php
@@ -1,3 +1,16 @@
 <?php
 
 require_once( __DIR__ . '/../vendor/autoload.php');
+
+?>
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<form id="newUserNameForm">
+    <input type="text" placeholder="Please enter your name" maxlength=100 id="nameEnterField"/>
+    <input type="submit" value="Submit"/>
+</form>
--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,27 @@
+function addNameFieldError(error) {
+    $('#nameEnterField').val('');
+    $('#nameEnterField').addClass('redBorder');
+    $('#nameEnterField').attr('placeholder', error);
+}
+
+function validateNameField() {
+    var name = $('#nameEnterField').val();
+    if(name.length < 1) {
+        addNameFieldError("Must be entered");
+    }
+    if(name.length >= 10) {
+        addNameFieldError("Fewer than 100 characters");
+    }
+}
+
 $(document).ready(function() {
-        $('#scanButton').click(function() {
-            window.open('server/scan.php');
-        });
+    $('#scanButton').click(function() {
+        window.open('server/scan.php');
+    });
+
+    $('#newUserNameForm').submit(function(event) {
+        validateNameField();
+        event.preventDefault();
+        return false;
+    });
 });
